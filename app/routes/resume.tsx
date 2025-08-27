37dd7329289ecb92cbe9f69fa74934b61511efcf
iman <imancx.cx@gmail.com>
2025-08-27 22:50:58 +0330

    add ATS component to resume.tsx

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,7 +1,10 @@
-import { Link, useNavigate, useParams } from "react-router";
 import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router";
+
 import { usePuterStore } from "~/lib/puter";
+
 import Summary from "~/components/Summary";
+import ATS from "~/components/ATS";
 
 export const meta = () => [
   { title: "Resumind | Review " },
@@ -77,6 +80,10 @@ const Resume = () => {
           {feedback ? (
             <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
               <Summary feedback={feedback} />
+              <ATS
+                score={feedback.ATS.score || 0}
+                suggestions={feedback.ATS.tips || []}
+              />
             </div>
           ) : (
             <img src="/images/resume-scan-2.gif" className="w-full" />
